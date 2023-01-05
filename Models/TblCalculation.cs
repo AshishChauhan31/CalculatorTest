ace24bd3f885a849911b85c3da0535708c5f7a54
Ashish <ashish.chauhan_mca18@gla.ac.in>
2023-01-06 02:16:13 +0530

    First Commit. Code Pushed

---
+++ Models/TblCalculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculatorTest.Models;
+
+public partial class TblCalculation
+{
+    public int Id { get; set; }
+
+    public string? Input { get; set; }
+
+    public string? FinalResult { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+}
